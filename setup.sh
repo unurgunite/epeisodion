6825457e8d55d325feb04e79ef8030d5c4ba7845
unurgunite <senpaiguru1488@gmail.com>
2022-02-28 23:33:56 +0300

    Update `show_license` function in `setup.sh`

--- setup.sh
+++ setup.sh
@@ -77,13 +77,29 @@ move_script() {
 
 #-------------------- Show LICENSE.txt file -------------------------------------------------------#
 show_license() {
-  if [ ! -s ${License_File} ]; then
-    echo "No license files were found! Please, update your local repo from $GitHub_Repo."
-    return 10
-  else
-    cat "$License_File"
-  fi
-  return 0
+  cat <<LICENSE
+  MIT License
+  
+  Copyright (c) 2022 unurgunite
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+LICENSE
 }
 
 #-------------------- Get parameters --------------------------------------------------------------#
