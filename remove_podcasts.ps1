6fe6603fa7e9a34848933305776a526322896370
unurgunite <senpaiguru1488@gmail.com>
2022-02-28 22:30:43 +0300

    Update docs & Refactor source for `remove_podcasts.ps1`

--- remove_podcasts.ps1
+++ remove_podcasts.ps1
@@ -1,7 +1,9 @@
-Set-Variable exe_dir -Option ReadOnly -Value "%appdata%/Spotify/Apps/"
-Set-Variable ms_dir -Option ReadOnly -Value "%ProgramFiles%/WindowsApps"
-Set-Variable package -Option ReadOnly -Value "SpotifyAB.SpotifyMusic"
-
+# -------------------------------------------------------------------------------------------------
+# Function name: Downloaded-From-MStore
+# -------------------------------------------------------------------------------------------------
+# Semantics:
+# Function-predicate to check if Spotify is stored in std MS Store folder
+# -------------------------------------------------------------------------------------------------
 function Downloaded-From-MStore
 {
     if (Get-AppxPackage -Name $package)
@@ -10,17 +12,35 @@ function Downloaded-From-MStore
     }
 }
 
+# -------------------------------------------------------------------------------------------------
+# Function name: Stored-In-ProgramFiles-Folder
+# -------------------------------------------------------------------------------------------------
+# Semantics:
+# Function-predicate to check if Spotify is stored in std Program Files folder
+# -------------------------------------------------------------------------------------------------
+# Return:
+# True                  - If Spotify exists in default programs folder
+# -------------------------------------------------------------------------------------------------
 function Stored-In-ProgramFiles-Folder
 {
-    if (Test-Path -Path $exe_dir)
+    if (Test-Path -Path $std_prog_dir)
     {
         return $true
     }
 }
 
+# -------------------------------------------------------------------------------------------------
+# Function name: File-Exists
+# -------------------------------------------------------------------------------------------------
+# Semantics:
+# Function-predicate to check if file exists
+# -------------------------------------------------------------------------------------------------
+# Return:
+# True                  - If file exists
+# -------------------------------------------------------------------------------------------------
 function File-Exists($path)
 {
-    if (Test-Path -Path $path -eq $true)
+    if ((Test-Path -Path $path) -eq $true)
     {
         return $true
     }
@@ -31,63 +51,122 @@ function File-Exists($path)
     }
 }
 
-
-function Add-File-To-Archive($archive, $file)
+# -------------------------------------------------------------------------------------------------
+# Function name: Stop-Spotify-Process
+# -------------------------------------------------------------------------------------------------
+# Semantics:
+# It kills all Spotify processes
+# -------------------------------------------------------------------------------------------------
+function Stop-Spotify-Process
 {
-    clear-host
-    Add-Type -AssemblyName System.IO.Compression
-    Add-Type -AssemblyName System.IO.Compression.FileSystem
-
-    [System.IO.Compression.ZipArchive]$ZipFile = [System.IO.Compression.ZipFile]::Open($archive, ([System.IO.Compression.ZipArchiveMode]::Update))
-    [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($ZipFile, $file, (Split-Path $file -Leaf))
-    $ZipFile.Dispose()
+    $spotify = Get-Process Spotify -ErrorAction SilentlyContinue
+    if ($spotify)
+    {
+        $spotify.CloseMainWindow()
+        Sleep 5
+        if (!$spotify.HasExited)
+        {
+            $spotify | Stop-Process -Force
+        }
+    }
+    Remove-Variable spotify
 }
 
-function Edit-Source($path, $ouput_dir)
+# -------------------------------------------------------------------------------------------------
+# Function name: Edit-Source
+# -------------------------------------------------------------------------------------------------
+# Semantics:
+# It edits source of the xpui.js file from `spa`-archive
+# -------------------------------------------------------------------------------------------------
+function Edit-Source($path)
 {
-    Add-Type -AssemblyName System.IO.Compression.FileSystem
-    $Name = 'xpui.js'
-    $zip = [System.IO.Compression.ZipFile]::OpenRead($path)
-    $zip.Entries |
-            Where-Object { $_.FullName -like $Name } |
-            ForEach-Object {
-                $FileName = $_.Name
-                [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$ouput_dir", $true)
-            }
-    $zip.Dispose()
-    (Get-Content "$path/$Name") -replace ',show,', ',' | Set-Content "$path/$Name"
-    (Get-Content "$path/$Name") -replace ',episode${i}', '' | Set-Content "$path/$Name"
-    Add-File-To-Archive($path, "$path/$Name")
+    Add-Type -Assembly 'System.IO.Compression.FileSystem'
+    $zip = [System.IO.Compression.ZipFile]::Open($path, 'update')
+    $entry = $zip.GetEntry('xpui.js')
+
+    $reader = New-Object System.IO.StreamReader($entry.Open())
+    $xpuiContents = $reader.ReadToEnd()
+    $reader.Close()
+    if ($xpuiContents)
+    {
+        $xpuiContents = $xpuiContents -replace '\,show\,', '\,'
+        $xpuiContents = $xpuiContents -replace '\,episode\${i}', ''
+
+        $writer = New-Object System.IO.StreamWriter($entry.Open())
+        $writer.BaseStream.SetLength(0)
+        $writer.Write($xpuiContents)
+        $writer.Close()
+        $zip.Dispose()
+    }
 }
 
+# -------------------------------------------------------------------------------------------------
+# Function name: Unzip-Package
+# -------------------------------------------------------------------------------------------------
+# Semantics:
+# It checks for installation source of Spotify and invoke other functions to unzip source files
+# -------------------------------------------------------------------------------------------------
+# Constants:
+# $package              - Default package name
+# $std_prog_dir         - Default programs folder
+# $ms_progs_dir         - Default MS Store programs folder
+# $ms_spotify_dir       - Default MS Store Spotify folder
+# $ms_spa_file          - Default MS Store Spotify xpui.spa filename
+# $ms_spa_file_bak      - Default MS Store Spotify xpui.spa backup filename
+# $std_spa_file         - Default non-MS Store Spotify xpui.spa filename
+# $std_spa_bak          - Default non-MS Store Spotify xpui.spa backup filename
+# -------------------------------------------------------------------------------------------------
+# Return:
+# 1                     - If xpui.spa.bak does not exist
+# 10                    - If no Spotify binaries were found
+# -------------------------------------------------------------------------------------------------
 function Unzip-Package
 {
+    Set-Variable package            -Option ReadOnly -Value "SpotifyAB.SpotifyMusic"
+    Set-Variable std_prog_dir       -Option ReadOnly -Value "$env:APPDATA\Spotify\Apps"
+    Set-Variable ms_progs_dir       -Option ReadOnly -Value "$env:PROGRAMFILES\WindowsApps"
+    Set-Variable ms_spotify_dir     -Option ReadOnly -Value "$env:PROGRAMFILES\WindowsApps\$package"
+    Set-Variable ms_spa_file        -Option ReadOnly -Value "$ms_spotify_dir\xpui.spa"
+    Set-Variable ms_spa_file_bak    -Option ReadOnly -Value "$ms_spa_file.bak"
+    Set-Variable std_spa_file       -Option ReadOnly -Value "$std_prog_dir\xpui.spa"
+    Set-Variable std_spa_bak        -Option ReadOnly -Value "$std_spa_file.bak"
     if (Downloaded-From-MStore)
     {
-        $archive_dir = "$ms_dir/$package/Apps"
-        if (File-Exists("$exe_dir/xpui.xpa"))
+        if (File-Exists("$ms_spa_file"))
         {
-            Copy-Item "$archive_dir/xpui.xpa" -Destination "$archive_dir/xpui.xpa.bak"
+            Copy-Item "$ms_spa_file" -Destination "$ms_spa_file_bak"
         }
-        File-Exists("$exe_dir/xpui.xpa.bak")
-        Edit-Source("$archive_dir/xpui.xpa", "$archive_dir")
+        File-Exists("$ms_spa_file_bak")
+        Edit-Source("$ms_spa_file")
     }
     elseif (Stored-In-ProgramFiles-Folder)
     {
-        if (File-Exists("$exe_dir/xpui.xpa"))
+        if (File-Exists("$std_spa_file"))
         {
-            Copy-Item "$archive_dir/xpui.xpa" -Destination "$archive_dir/xpui.xpa.bak"
+            Copy-Item "$std_spa_file" -Destination "$std_prog_dir\$std_spa_bak"
         }
-        Copy-Item "$exe_dir/xpui.xpa" -Destination "$exe_dir/xpui.xpa.bak"
-        if (Test-Path -Path "$exe_dir/xpui.xpa.bak" -eq $false)
+        Copy-Item "$std_spa_file" -Destination "$std_prog_dir\$std_spa_bak"
+        if ((Test-Path -Path "$std_prog_dir\$std_spa_bak") -eq $false)
         {
+            Write-Output "Can not create $std_prog_dir\$std_spa_bak file"
             return 1
         }
-        Edit-Source("$exe_dir/xpui.xpa", "$exe_dir")
+        Edit-Source("$std_spa_file")
     }
-    Stop-Process -Name "Spotify"
+    else
+    {
+        Write-Output "Can not find Spotify app. Is it stored on your PC?"
+        return 10
+    }
+    Stop-Spotify-Process
 }
 
+# -------------------------------------------------------------------------------------------------
+# Function name: main
+# -------------------------------------------------------------------------------------------------
+# Semantics:
+# Entrypoint for the program
+# -------------------------------------------------------------------------------------------------
 function main
 {
     Unzip-Package
